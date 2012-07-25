19f43856d5f67725a825106fd8f5b674e760ae60
Gareth Tribello <gareth.tribello@gmail.com>
2012-07-25 15:18:21 +0200

    Added nicer error detection when using SWITCH keyword

--- src/ColvarCoordination.cpp
+++ src/ColvarCoordination.cpp
@@ -105,8 +105,10 @@ reduceListAtNextStep(false)
 
   string sw,errors;
   parse("SWITCH",sw);
-  if(sw.length()>0) switchingFunction.set(sw,errors);
-  else {
+  if(sw.length()>0){
+      switchingFunction.set(sw,errors);
+      if(errors.length()!=0) error("problem reading SWITCH keyword : " + errors);
+  } else {
     int nn=6;
     int mm=12;
     double d0=0.0;
