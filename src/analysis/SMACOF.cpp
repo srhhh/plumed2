e34ea5d2f988e3cc00645499163a24d89cc66576
Gareth Tribello <gt@eider.asg>
2013-11-21 14:52:23 +0000

    A few small fixes to smacof algorithm

--- src/analysis/SMACOF.cpp
+++ src/analysis/SMACOF.cpp
@@ -26,24 +26,7 @@ namespace PLMD {
 namespace analysis {
 
 void SMACOF::run( PointWiseMapping* mymap ){
-//   double half=(-0.5); Matrix<double> Distances( half*mymap->modifyDmat() );
-//   int M=Distances.nrows(); 
-//   Matrix<double> Ones( M, M);
-//   for(unsigned i=0; i<M; ++i) { 
-//      for(unsigned j=0; j<M; ++j) {
-//          Ones(i,j)=-1./(static_cast<double>(M) );         //off diagonal elements of J
-//       }
-//   } for(unsigned i=0; i<M; ++i) Ones(i,i)=1-1./(static_cast<double>(M) );   //diagonal elements of J
-// 
-//    Matrix<double> tmp( M, M ), tmp2( M, M ); 
-//    mult( Ones, Distances, tmp ); mult( tmp, Ones, tmp2 );
-// 
-//    std::vector<double> eigval(M); Matrix<double> eigvec(M,M);
-//    diagMat( tmp2, eigval, eigvec );
-//    //after diagonalising the matrix, construct initial Z matrix
-//   
-    
-
+    printf("In SMACOF run\n");
     Matrix<double> Distances( mymap->modifyDmat() ); unsigned M = Distances.nrows();
     Matrix<double> InitialZ( M, mymap->getNumberOfProperties() );     //want Mx2 Z matrix but you set columns to M dimension first and then changes to 2 underneath
     for(unsigned i=0; i<M; ++i){
@@ -51,45 +34,45 @@ void SMACOF::run( PointWiseMapping* mymap ){
             InitialZ(i,j) =  mymap->getProjectionCoordinate( i, j );    
        }
     }
+    printf("Retrieved projection coordinates\n");
 
     double myfirstsig = calculateSigma( Distances, InitialZ );    //this is a function that outputs the initial sigma values
     // initial sigma is made up of the original distances minus the distances between the projections all squared.
     unsigned MAXSTEPS=100; double tol=1.E-4; Matrix<double> BZ( M, M ), newZ( M, mymap->getNumberOfProperties() );
     for(unsigned i=0;i<MAXSTEPS;++i){
         if(i==MAXSTEPS-1) plumed_merror("ran out of steps in SMACOF algorithm");
-        
-    // Calculate B(Z)  which is a M x M square matrix
-    //Off diagonals considered first 
-    Matrix<double> BZ(M,M); //create the space memory for the MxM matrix
-    for(unsigned i=0; i<M; ++i){
-       for(unsigned j=0; j<i; ++j){
-        if(i=j) continue;  //skips over the diagonal elements
-        //Calculate the distance between point i and point j.
-        //Create the space memory for a scalar, as this is what the distance is.
-        double dist=0;
-        for(unsigned k=0; k<mymap->getNumberOfProperties(); ++k){  //mymap->getNumberOfProperties() is the range that the columns in InitialZ go up to therefore we use it here also
-        double tmp=InitialZ(i,k) - Initial(j,k);
-        dist+=tmp*tmp //this is squaring the line above. Quicker way to do things than to actually write tmp^2
-        //off diagonal elements are [-1x(dissimilarities/distances)]/M i.e. BZ isn't symmetric
-        //If it was symmetric that line would simply read BZ(i,j)=BZ(j,i)=sqrt(dist)
-        BZ(i,j)=-(Distances(i,j) / sqrt(dist)) / (static_cast<double>(M)); 
-        //static_cast<double>(M) is the M that we want to dive by in the code 
+        printf("Doing step %d of SMACOF %d %d \n",i,M,mymap->getNumberOfProperties() );        
+
+        // Calculate B(Z)  which is a M x M square matrix
+        //Off diagonals considered first 
+        // Matrix<double> BZ(M,M); //create the space memory for the MxM matrix
+        for(unsigned i=0; i<M; ++i){
+           for(unsigned j=0; j<M; ++j){
+            if(i==j) continue;  //skips over the diagonal elements
+            //Calculate the distance between point i and point j.
+            //Create the space memory for a scalar, as this is what the distance is.
+            double dist=0;
+            for(unsigned k=0; k<mymap->getNumberOfProperties(); ++k){  //mymap->getNumberOfProperties() is the range that the columns in InitialZ go up to therefore we use it here also
+              double tmp=InitialZ(i,k) - InitialZ(j,k);
+              dist+=tmp*tmp; //this is squaring the line above. Quicker way to do things than to actually write tmp^2
+            } 
+            //off diagonal elements are [-1x(dissimilarities/distances)]/M i.e. BZ isn't symmetric
+            //If it was symmetric that line would simply read BZ(i,j)=BZ(j,i)=sqrt(dist)
+            BZ(i,j)=-(Distances(i,j) / sqrt(dist)) / (static_cast<double>(M)); 
+            //static_cast<double>(M) is the M that we want to dive by in the code 
+           }
+           //the diagonal elements are -off diagonal elements BZ(i,i)-=BZ(i,j)   (Equation 8.25)
+           BZ(i,i)=0; //create the space memory for the diagonal elements which are scalars
+           for(unsigned j=0; j<M; ++j){ 
+              if(i==j) continue;
+              BZ(i,i)-=BZ(i,j); 
+           }
         }
-       }
-    } 
-    //the diagonal elements are -off diagonal elements BZ(i,i)-=BZ(i,j)   (Equation 8.25)
-    BZ(i,i)=0 //create the space memory for the diagonal elements which are scalars
-    for(unsigned i=0; i<M; ++i){
-       for(unsigned j=0; j<i; ++j){
-       BZ(i,i)-=BZ(i,j);
-       }
-    }
-       
-       
        // Matrix matrix multiply B(Z) times Z and multiply by 1/M. This is the Guttman transform for w(ij)=1, which calculates X which we know is exactly equal to the newZ value
         mult( BZ, InitialZ, newZ ); 
         //Compute new sigma
         double newsig = calculateSigma( Distances, newZ );
+        printf("CHECKING VALUE OF SIGMA %d %f %f %f \n",i,myfirstsig,newsig,myfirstsig-newsig);
         //Computing whether the algorithm has converged (has the mass of the potato change
         //when we put it back in the oven!)
         if( fabs( newsig - myfirstsig )<tol ) break;    //An f means real and by convention is put in before abs. abs means absolute value.
@@ -103,19 +86,20 @@ void SMACOF::run( PointWiseMapping* mymap ){
     for(unsigned i=0;i<M;++i){
         for(unsigned j=0;j<mymap->getNumberOfProperties();++j) mymap->setProjectionCoordinate( i, j, newZ(i,j) ); 
         //is the line above right? i havent changed it but i dont think it is right.
-        } 
     }
+
+}
 
 double SMACOF::calculateSigma( const Matrix<double>& Distances, const Matrix<double>& InitialZ ){    //& sign always put in a function as routine but dont need to know why
     unsigned M = Distances.nrows();
     double sigma=0;  //Sigma is a scalar hence why it needs to be written in this format
     for(unsigned i=1; i<M; ++i){
        for(unsigned j=0; j<i; ++j){
-       double dlow=0  //dlow are the distances in the lower triangular part of the matrix. Don't need to include all distances because symmentric
-          for(unsigned k=0; k<.....; ++k){
+       double dlow=0;  //dlow are the distances in the lower triangular part of the matrix. Don't need to include all distances because symmentric
+          for(unsigned k=0; k<InitialZ.ncols();++k){
           //this is doing the summation again with ... being the highest value in the summation.
           double tmp2=InitialZ(i,k) - InitialZ(j,k); //this is the maths within the summation sign
-          dlow+=tmp2*tmp2
+          dlow+=tmp2*tmp2;
           }
        double tmp3= Distances(i,j) - sqrt(dlow);
        sigma+= tmp3*tmp3;   
